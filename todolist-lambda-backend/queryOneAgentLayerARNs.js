0b00432a0ae0ceb6bfe3b98642d2ef210e6fd9ab
Hollerberg <17945993+Hollerberg@users.noreply.github.com>
2021-04-26 15:48:35 +0200

    rename + improvements

---
+++ todolist-lambda-backend/queryOneAgentLayerARNs.js
@@ -0,0 +1,146 @@
+const https = require('https');
+
+/**
+ * determine latest OneAgent extension layer ARNs
+ * preconditions:
+ * - the new serverless framework asychnronous variable resolver must be enabled. in order to do so,
+ *   add `variablesResolutionMode: '20210326'` to the toplevel of your serverless.yml file.
+ * - DT_PAAS_TOKEN environment varible contains PaaS token required to query deployment API (alternatively,
+ *   specify custom.queryOneAgentLayerARNs.paasToken in serverless configuration)
+ * - OneAgent configuration - expected to be copied custom.OneAgentConfig
+ *
+ * An excerpt of a sample serverless.yml using queryOneAgentLayerARNs.js
+ *
+ *  functions:
+ *    helloWorld:
+ *      handler: index.hello
+ *      # reference custom.OneAgentlayerARNs.nodejs to include OneAgent layer
+ *      layers: ${self:custom.OneAgentlayerARNs.nodejs}
+ *      # reference custom.OneAgentConfig with OneAgent configuration from Lambda deployment screen
+ *      environment: ${self:custom.OneAgentConfig}
+ *
+ *   custom:
+ *     # specify PAAS token. Alternatively, specify environment variable DT_PAAS_TOKEN
+ *     queryOneAgentLayerARNs:
+ *       paasToken: <PAAS token>
+ *
+ *     # OneAgentLayerARNs will be resolved to the latest version of OneAgent layer ARNs e.g.
+ *     # OneAgentlayerARNs:
+ *     #   python: >-
+ *     #     arn:aws:lambda:us-east-1:725887861453:layer:Dynatrace_OneAgent_1_217_1_python:1
+ *     #   java: >-
+ *     #     arn:aws:lambda:us-east-1:725887861453:layer:Dynatrace_OneAgent_1_217_10_java:1
+ *     #   nodejs: >-
+ *     #      arn:aws:lambda:us-east-1:725887861453:layer:Dynatrace_OneAgent_1_217_1_nodejs:1
+ *     OneAgentlayerARNs: ${file(./queryOneAgentLayerARNs.js):get}
+ *
+ *     # settings copied from Dynatrace Lambda deployment screen
+ *     OneAgentConfig:
+ *       AWS_LAMBDA_EXEC_WRAPPER: /opt/dynatrace
+ *       DT_TENANT: xyzsfsdf
+ *       DT_CLUSTER_ID: 2041375367
+ *       DT_CONNECTION_BASE_URL: https://xyzsfsdf.live.dynatrace.com
+ *       DT_CONNECTION_AUTH_TOKEN: xxxxxx.xxxxxxxx.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+ *
+ * @returns JSON document with layer name for every supported runtime
+ */
+module.exports.get = async (sls) => {
+  // lookup paas token from custom.queryOneAgentLayerARNs.paasToken
+  let paasToken = await sls.resolveConfigurationProperty([
+    'custom',
+    'queryOneAgentLayerARNs',
+    'paasToken',
+  ]);
+
+  if (paasToken == null) {
+    // paas token not in defined in config file -> try environment
+    paasToken = process.env.DT_PAAS_TOKEN;
+  }
+
+  if (paasToken == null) {
+    throw new Error(
+      'neither custom.queryOneAgentLayerARNs.paasToken nor DT_PAAS_TOKEN environment variable defined'
+    );
+  }
+
+  // resolve region and connection base url
+  const [region, connetionBaseUrl] = await Promise.all([
+    sls.resolveConfigurationProperty(['provider', 'region']),
+    sls.resolveConfigurationProperty([
+      'custom',
+      'OneAgentConfig',
+      'DT_CONNECTION_BASE_URL',
+    ]),
+  ]);
+
+  if (region == null) {
+    return Promise.reject(new Error('could not resolve AWS region'));
+  }
+
+  if (connetionBaseUrl == null) {
+    return Promise.reject(
+      new Error(
+        'could not resolve DT_CONNECTION_BASE_URL from custom.OneAgentConfig'
+      )
+    );
+  }
+
+  return new Promise((resolve, reject) => {
+    const opts = {
+      headers: {
+        accept: '*/*',
+        Authorization: `Api-Token ${paasToken}`,
+      },
+    };
+
+    https.get(
+      `${connetionBaseUrl}/api/v1/deployment/agent/lambda/latest`,
+      opts,
+      (res) => {
+        if (res.statusCode !== 200) {
+          reject(
+            new Error(
+              `Could not retrieve OneAgent layer names - request failed with ${res.statusCode}`
+            )
+          );
+        }
+        let data = '';
+        res.on('data', (chunk) => {
+          data += `${chunk}`;
+        });
+        res.on('end', () => {
+          try {
+            // version is a Record with runtime name as key and according partial layername
+            const { versions } = JSON.parse(data);
+
+            // transform the partial layer names to full ARNs
+            const transformed = {};
+            Object.getOwnPropertyNames(versions).forEach((runtime) => {
+              const partialLayerName = versions[runtime];
+              transformed[
+                runtime
+              ] = `arn:aws:lambda:${region}:725887861453:layer:${partialLayerName}_${runtime}:1`;
+            });
+            resolve(transformed);
+          } catch (e) {
+            reject(new Error(`could not parse layer names - ${e}`));
+          }
+        });
+        res.on('error', reject);
+      }
+    );
+  });
+};
+
+/**
+ * export runtime specific layer ARN
+ * returns the OneAgent layer ARN for a specific runtime technology.
+ *
+ * @see queryOneAgentLayerARNs for configuration and preconditions.
+ */
+['nodejs', 'python', 'java'].forEach((runtime) => {
+  module.exports[runtime] = async (sls) => {
+    const layerARNs = await queryOneAgentLayerARNs(sls);
+    return layerARNs[runtime];
+  };
+});
